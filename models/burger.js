a4998331cc19c147970e2fe49d6550c9dc45bf93
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-04 08:39:58 -0500

    added delete function to models

--- models/burger.js
+++ models/burger.js
@@ -22,6 +22,11 @@ var burger = {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
   }
 };
 
