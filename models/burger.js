ad96c1b22e1a183a1d7d24dddc634be0f2713311
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-04 09:33:30 -0500

    update to models

--- models/burger.js
+++ models/burger.js
@@ -3,31 +3,26 @@ var orm = require("../config/orm.js");
 
 // Create the burger object
 var burger = {
-  // Select all burger table entries
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  selectAll: function(cb) {
+    orm.selectAll(function(res) {
       cb(res);
     });
   },
-
-  // The variables cols and vals are arrays
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  insertOne: function(burger, cb) {
+    orm.insertOne(burger, function(res) {
       cb(res);
     });
   },
-
-  // The objColVals is an object specifying columns as object keys with associated values
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  updateOne: function(id, cb) {
+    orm.updateOne([id], function(res) {
       cb(res);
     });
   }
+  // delete: function(condition, cb) {
+  //   orm.delete("burgers", condition, function(res) {
+  //     cb(res);
+  //   });
+  // }
 };
 
 // Export the database functions for the controller (burgerController.js).
