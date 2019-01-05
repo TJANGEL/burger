44cffce802424dbb7e01d370874d486a6188dcb7
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-05 12:40:50 -0500

    -update to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,32 +5,36 @@ var router = express.Router();
 
 var burger = require("../models/burger");
 
+// get all burgers
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
+    // console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
+// create a new burger
 router.post("/burgers", function(req, res) {
   burger.insertOne(req.body.burger_name, function() {
     res.redirect("/");
   });
 });
 
+// update the list of burgers
 router.put("/burgers/:id", function(req, res) {
   var id = req.params.id;
 
-  console.log("id", id);
+  // console.log("id", id);
 
   burger.updateOne(id, function() {
     res.redirect("/");
   });
 });
 
+// delete a devoured burger
 // router.delete("/api/burgers/:id", function(req, res) {
 //   var condition = "id = " + req.params.id;
 
